eab04d883258b4c2b5f7e6a99d48062846e0fa1a
Andrew Albrecht <andrewalbrecht@utexas.edu>
2018-11-13 19:12:38 -0600

    added math for train frequency

--- javascript.js
+++ javascript.js
@@ -30,7 +30,9 @@ firebase.initializeApp(config);
     let newEmployee = $("<td>" + employeeName+"</td>");
     let newRole = $("<td>" + EmpRole+"</td>");
     let monthsWorked = $("<td>" + dateConverted+"</td>");
-    let newStartDate = $("<td>" + startDate+"</td>");          
+    let newStartDate = $("<td>" + startDate+"</td>");   
+    
+    
     let newRate = $("<td>" + monthlyRate+"</td>");
     let newBill = $("<td>" + finalTotalBilled+"</td>");
     newRow.append(newEmployee).append(newRole).append(newStartDate).append(monthsWorked).append(newRate).append(newBill);
@@ -39,6 +41,48 @@ firebase.initializeApp(config);
 
     console.log(childSnapshot.val());
     console.log(childSnapshot.val().startDate)
+
+    // Assumptions
+
+    // Time is 3:30 AM
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(startTime, "HH:mm").subtract(1, "days");
+    console.log(firstTimeConverted);
+
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+
+    // Time apart (remainder)
+    var remainder = diffTime % frequency;
+    console.log(frequency);
+    console.log(remainder);
+
+
+    //last field for minutes left=================================================
+    // Minute Until Train
+    var minutesTillTrain = frequency - remainder;
+    //append to minTilTrain
+    //===============================================
+    console.log("MINUTES TILL TRAIN: " + minutesTillTrain);
+
+
+        //next arrivel field=================================================
+
+    // Next Train
+    var nextTrain = moment().add(minutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+    // push to new name of variable nextArrival
+
+
+    //next arrivel field=================================================
+
 
   });
   
