6df60757d4524519516f270cd86d92de696ea2f9
danielpolk <danielpolk@mac.com>
2018-12-01 11:42:48 -0600

    added comments

--- javascript.js
+++ javascript.js
@@ -9,40 +9,39 @@ var config = {
 };
 firebase.initializeApp(config);
   
+// Assign the reference to the database to a variable named 'database'
+var database = firebase.database();
+
 //Submit client side data to firebase.
 $("#submit-form").on("click", function (event) {
   event.preventDefault();
-
+//Get user input value and store in variables.
   trainName = $("#train-name").val().trim();
   trainDestination = $("#train-destination").val().trim();
   startTime = $("#start-time").val().trim();
   frequency = $("#frequency").val().trim();
-    
+//Creating a timestamp that is the current time.
   let timestamp = Date.now();
-
-
+//Clear divs.
   $("#train-name").val("");
   $("#train-destination").val("");
   $("#start-time").val("");
   $("#frequency").val("");
 
 
-
-database.ref().push({
-name: trainName,
-destination: trainDestination,
-startTime: startTime,
-dateAdded: timestamp,
-frequency: frequency,
-})
+//Connect to database...pushing object with key and value.
+  database.ref().push({
+  name: trainName,
+  destination: trainDestination,
+  startTime: startTime,
+  dateAdded: timestamp,
+  frequency: frequency,
+  })
 
 });   
 
 
 
-
-// Assign the reference to the database to a variable named 'database'
-var database = firebase.database();
 
 database.ref().orderByChild("dateAdded").limitToLast(3).on("child_added", function(childSnapshot){
 
@@ -53,8 +52,9 @@ database.ref().orderByChild("dateAdded").limitToLast(3).on("child_added", functi
 
 
   console.log(childSnapshot.val());
-  
+  //Turn user input of time (23:00) timeArrival = using .split = [23, 00]
   var timeArrival = startTime.split(":");
+  //
   var trainTime = moment().hours(timeArrival[0]).minutes(timeArrival[1]);
   var maxMoment = moment.max(moment(), trainTime);
 
